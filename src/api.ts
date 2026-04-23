4e77e2aeef3357b1c6bae349d5f0482ec3b2317d
Sanidhya ST <st.playascend@gmail.com>
2026-04-23 22:49:26 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const BASE = "http://192.168.137.1:8001";
+const BASE_URL = "http://3.110.170.150:8000";
 
 export const api = axios.create({
   baseURL: `${BASE}/api`,
