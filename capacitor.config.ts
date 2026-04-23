8669d9c2dfe56ca2a6b71c35a1347183de571465
stplayascend <st.playascend@gmail.com>
2026-04-23 22:35:25 +0530

    frontend for build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourname.attendance',
+  appName: 'Attendance App',
+  webDir: 'build'
+};
+
+export default config;
